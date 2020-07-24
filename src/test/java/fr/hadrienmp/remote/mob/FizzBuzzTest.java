88945bbad5ea2e352f6788f80d19f5b9a4b0ca11
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-07-24 11:34:20 +0200

    Add maven stuff

--- src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
+++ src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
@@ -1,13 +1,16 @@
 package fr.hadrienmp.remote.mob;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
     @ParameterizedTest
-    @ValueSource(ints = {1})
+    @CsvSource({
+            "1"
+    })
     void return_the_number_otherwise(int n) {
         assertThat(fizzbuzz(n)).isEqualTo("1");
     }

5337eadd5034749aacd4359a09071e849616100f
srezzouqi <srezzouqi@financeactive.com>
2020-07-24 12:07:34 +0200

    empty message

--- src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
+++ src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
@@ -58,6 +58,6 @@ public class FizzBuzzTest {
         if(n  % 3 == 0 && n % 5 == 0) return "FizzBuzz";
         if(n % 5 == 0) return "Buzz";
         if (n % 3 == 0) return "Fizz";
-        return String.valueOf(n);
+        return String.valueOf(n);//
     }
 }

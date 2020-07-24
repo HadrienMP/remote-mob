ab1fe92df95903008c83e587a6990d6fbb059eed
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-07-24 11:35:21 +0200

    Add maven stuff

--- src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
+++ src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
@@ -9,13 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzTest {
     @ParameterizedTest
     @CsvSource({
-            "1,1"
+            "1,1",
+            "2,2"
     })
     void return_the_number_otherwise(int n, String expected) {
         assertThat(fizzbuzz(n)).isEqualTo(expected);
     }
 
     private String fizzbuzz(int n) {
-        return "1";
+        return String.valueOf(n);
     }
 }

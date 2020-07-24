4ae664743b43aa55245919750b944df68d9b1bf8
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-07-24 11:49:17 +0200

    Add maven stuff

--- src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
+++ src/test/java/fr/hadrienmp/remote/mob/FizzBuzzTest.java
@@ -32,7 +32,7 @@ public class FizzBuzzTest {
     }
 
     private String fizzbuzz(int n) {
-        if(n == 5 || n == 10 || n == 20) return "Buzz";
+        if(n % 5 == 0) return "Buzz";
         if (n % 3 == 0) return "Fizz";
         return String.valueOf(n);
     }

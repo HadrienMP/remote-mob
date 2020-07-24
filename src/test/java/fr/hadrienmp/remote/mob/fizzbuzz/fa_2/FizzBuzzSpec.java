4c88713a15341eb03fd84008804f933bec9daf9c
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-07-24 14:06:01 +0200

    Add maven stuff

--- src/test/java/fr/hadrienmp/remote/mob/fizzbuzz/fa_2/FizzBuzzSpec.java
+++ src/test/java/fr/hadrienmp/remote/mob/fizzbuzz/fa_2/FizzBuzzSpec.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzSpec {
     @Test
-    void name() {
+    void one() {
         assertThat("1").isEqualTo("1");
     }
 }
